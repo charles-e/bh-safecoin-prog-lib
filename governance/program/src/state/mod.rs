0fce7d293a87d502a0f6face8f0e324e23fddd5a
sol <safe@vmi504578.contaboserver.net>
2021-08-06 17:50:59 +0200

    clear

--- governance/program/src/state/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Program accounts
-
-pub mod enums;
-pub mod governance;
-pub mod proposal;
-pub mod proposal_vote_record;
-pub mod realm;
-pub mod single_signer_instruction;
-pub mod voter_record;
--- governance/program/src/tools/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Utility functions
-
-pub mod account;
-
-pub mod token;
-
-pub mod asserts;
-
-pub mod bpf_loader_upgradeable;
